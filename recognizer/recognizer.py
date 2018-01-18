6d07cada595d6ccbb7b5dc1c1db5fbb227047abe
Julia Lucka <julia.lucka@ocado.com>
2018-01-18 23:15:44 +0100

    Added new type of rhymes to recognizer

--- recognizer/recognizer.py
+++ recognizer/recognizer.py
@@ -21,6 +21,15 @@ tren1 = "Wszytki płacze, wszytki łzy Heraklitowe \n" \
         "Nie wiem, co lżej : czy w smutku jawnie żałować,\n" \
         "Czyli się z przyrodzeniem gwałtem mocować?"
 
+toast = "Nie ma nic prócz tych liści, co na drzewach zmarły,\n" \
+        "Nic nie ma prócz tych wichrów, którymi przewiało,\n" \
+        "Nic prócz śladów świetności, co się już zatarły\n" \
+        "Nie stanie się nic więcej. Już wszystko się stało."
+
+koniec_wieku = "Przekleństwo-... tylko dziki, kiedy się skaleczy,\n" \
+               "złorzeczy swemu bogu, skrytemu w przestworze.\n" \
+               "Ironia-... Lecz największe z szyderstw czyż się może\n" \
+               "równać z ironią biegu najzwyklejszych rzeczy"
 
 class Recognizer:
     def analyze(self, text):
@@ -32,6 +41,8 @@ class Recognizer:
             else:
                 to_analyze.append(line.split()[-1])
         self.check_if_rymy_parzyste(to_analyze)
+        self.check_if_rymy_przeplatane(to_analyze)
+        self.check_if_rymy_okalajace(to_analyze)
 
     def check_if_rymy_parzyste(self, to_analyze):
         counter = 0
@@ -44,6 +55,28 @@ class Recognizer:
             print("Wiersz ma rymy parzyste")
         return counter / all_pairs > 0.5
 
+    def check_if_rymy_przeplatane(self, to_analyze):
+        counter = 0
+        all_pairs = 0
+        for (a, b) in zip(to_analyze[0:], to_analyze[2:]):
+            if self.are_rhymes(a, b):
+                counter += 1
+            all_pairs += 1
+        if counter / all_pairs > 0.5:
+            print("Wiersz ma rymy przeplatane")
+        return counter / all_pairs > 0.5
+
+    def check_if_rymy_okalajace(self, to_analyze):
+        counter = 0
+        all_fours = 0
+        for (a, b, c, d) in zip(to_analyze[0::4], to_analyze[3::4], to_analyze[1::4], to_analyze[2::4]):
+            if self.are_rhymes(a, b) and self.are_rhymes(c, d):
+                counter += 1
+            all_fours += 1
+        if counter / all_fours > 0.5:
+            print("Wiersz ma rymy okalające")
+        return counter / all_fours > 0.5
+
     def are_rhymes(self, a, b):
         counter = 0
         for (el_a, el_b) in zip(a[::-1], b[::-1]):
@@ -54,3 +87,5 @@ class Recognizer:
 
 recognizer = Recognizer()
 recognizer.analyze(tren1)
+recognizer.analyze(toast)
+recognizer.analyze(koniec_wieku)
