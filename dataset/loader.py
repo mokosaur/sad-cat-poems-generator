31099324c29388ebaa3d1ba75f7d851a69a2fd7a
Tomasz Danel <timages94@gmail.com>
2018-01-21 20:15:26 +0100

    Filter foreign languages and fix broken symbols

--- dataset/loader.py
+++ dataset/loader.py
@@ -3,7 +3,7 @@ import os
 __location__ = os.path.dirname(os.path.realpath(__file__))
 
 
-def load_author(author, files=None, keep_empty=False):
+def load_author(author, files=None, keep_empty=False, clean=True, only_polish=True):
     """Loads poems written by the given author.
 
     :param author: Author of the poems
@@ -24,10 +24,35 @@ def load_author(author, files=None, keep_empty=False):
                 clean_line = line.replace('\ufeff', '')
                 if clean_line.startswith("#"):
                     if len(content):
-                        poems[title] = content
+                        if clean:
+                            content = clean_data(content)
+                            title = clean_data(title)
+                        if not only_polish or polish_title(title):
+                            poems[title] = content
                         content = ''
                     title = clean_line[1:].strip()
                 else:
                     if keep_empty or clean_line.strip() != '':
                         content += clean_line
     return poems
+
+
+def clean_data(text):
+    corrections = {
+        'Ã³': 'ó',
+        'Å¼': 'ż',
+        'Ä™': 'ę',
+        'Å‚': 'ł',
+        'Å›': 'ś',
+        'Ä‡': 'ć',
+        'Ä…': 'ą',
+        'Å„': 'ń',
+        'Åº': 'ź'
+    }
+    for k, v in corrections.items():
+        text = text.replace(k, v)
+    return text
+
+
+def polish_title(title):
+    return "english" not in title and "esperanto" not in title and "[en]" not in title
