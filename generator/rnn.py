b36ff479e0fb4bcbecd06e0a029772664dda6130
Tomasz Danel <timages94@gmail.com>
2018-01-21 20:15:59 +0100

    Add parameters to LSTM model

--- generator/rnn.py
+++ generator/rnn.py
@@ -11,11 +11,16 @@ import os
 
 
 class LSTMGenerator(BaseGenerator):
-    def __init__(self, preprocessor=CharTokenizer(), line_by_line=False, sequence_length=100):
+    def __init__(self, preprocessor=CharTokenizer(), sequence_length=100,
+                 num_epochs=300, num_units=256, num_layers=2, batch_size=128, reverse_generation=True):
         BaseGenerator.__init__(self, preprocessor)
-        self.line_by_line = line_by_line
         self.sequence_length = sequence_length
         self.model = None
+        self.num_epochs = num_epochs
+        self.num_layers = num_layers
+        self.num_units = num_units
+        self.batch_size = batch_size
+        self.reverse_generation = reverse_generation
 
     def _fit_transformed(self, X):
         self.lookup = X['lookup']
@@ -27,32 +32,33 @@ class LSTMGenerator(BaseGenerator):
 
         # define the LSTM model
         self.model = Sequential()
-        self.model.add(LSTM(256, input_shape=(X_train.shape[1], X_train.shape[2])))
+        self.model.add(LSTM(self.num_units, input_shape=(X_train.shape[1], X_train.shape[2]),
+                            return_sequences=(self.num_layers > 1)))
         self.model.add(Dropout(0.2))
+        for i in range(self.num_layers - 1):
+            self.model.add(LSTM(self.num_units, return_sequences=(i < self.num_layers - 2)))
+            self.model.add(Dropout(0.2))
         self.model.add(Dense(y_train.shape[1], activation='softmax'))
         self.model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-        # define the checkpoint
         filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                 "models", "model-{epoch:02d}-{loss:.4f}.hdf5")
         checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
         callbacks_list = [checkpoint]
 
-        self.model.fit(X_train, y_train, epochs=20, batch_size=128, callbacks=callbacks_list)
+        self.model.fit(X_train, y_train, epochs=self.num_epochs, batch_size=self.batch_size, callbacks=callbacks_list)
 
     def _sample_lines(self, X):
         X_data = []
         y_data = []
         for x in X:
-            if self.line_by_line:
-                pass
-            else:
-                for i in range(len(x) - self.sequence_length):
-                    X_data.append([self.lookup[c.lower()] for c in x[i:i + self.sequence_length]])
-                    y_data.append(self.lookup[x[i + self.sequence_length].lower()])
+            x_rev = x[::-1] if self.reverse_generation else x
+            for i in range(len(x_rev) - self.sequence_length):
+                X_data.append([self.lookup[c.lower()] for c in x_rev[i:i + self.sequence_length]])
+                y_data.append(self.lookup[x_rev[i + self.sequence_length].lower()])
         return X_data, y_data
 
-    def generate(self, pattern, model_name=None, data=None, temperature=0.25):
+    def generate(self, pattern=None, model_name=None, data=None, temperature=0.25):
         if model_name:
             self.model = load_model(os.path.join(os.path.dirname(os.path.abspath(__file__)), "models", model_name))
         if data:
@@ -60,19 +66,23 @@ class LSTMGenerator(BaseGenerator):
             self.lookup = data['lookup']
             self.X = data['raw']
         lookup_inverse = {v: k for k, v in self.lookup.items()}
-        pattern = [self.lookup[x.lower()] for x in self.X[0][0:100]]
-        gentext = []
+        if not pattern:
+            pattern = self.X[0][:self.sequence_length]
+        if self.reverse_generation:
+            pattern = pattern[::-1]
+        gentext = [x for x in pattern]
+        pattern = [self.lookup[x.lower()] for x in pattern.zfill(self.sequence_length)]
         for i in range(1000):
             x = np.reshape(pattern, (1, len(pattern), 1))
             x = x / len(self.lookup)
             prediction = self.model.predict(x, verbose=0)
             index = self._get_prediction(prediction, temperature) if temperature else np.argmax(prediction)
             result = lookup_inverse[index]
-            seq_in = [lookup_inverse[value] for value in pattern]
-            # sys.stdout.write(result)
             gentext.append(result)
             pattern.append(index)
             pattern = pattern[1:len(pattern)]
+        if self.reverse_generation:
+            gentext = gentext[::-1]
         return ''.join(gentext)
 
     def _get_prediction(self, softmax, temperature):
